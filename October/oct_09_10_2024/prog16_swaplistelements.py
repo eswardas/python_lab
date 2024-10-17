661c2ac9cb8c6a7e3b913f81609cad47be4ec51d
eswardas <eswardaskn@gmail.com>
2024-10-17 20:06:06 +0530

    Update prog16_swaplistelements.py

--- October/oct_09_10_2024/prog16_swaplistelements.py
+++ October/oct_09_10_2024/prog16_swaplistelements.py
@@ -1,5 +1,12 @@
-lst = [34,56,3,12,78,5]
-length = len(lst)
+lst = []
+temp = 0
+length = int(input("Enter the size of the list:"))
+
+print(f"Enter {length} numbers:")
+for i in range(length):
+    temp = int(input())
+    lst.append(temp)
+
 print(f"List before swapping:{lst}")
 
 temp = lst[0]
@@ -7,8 +14,3 @@ lst[0] = lst[length-1]
 lst[length-1] = temp
 
 print(f"List after swapping:{lst}")
-
-
-
-
-
