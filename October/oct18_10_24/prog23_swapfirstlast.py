2577408a856f1fd171ea45c643bfedb8c043110f
eswardas <eswardaskn@gmail.com>
2024-10-18 11:56:55 +0100

    Update prog23_swapfirstlast.py

--- October/oct18_10_24/prog23_swapfirstlast.py
+++ October/oct18_10_24/prog23_swapfirstlast.py
@@ -1,7 +1,7 @@
 strr = input("Enter a string:")
 print(f"String before swapping:{strr}")
-strr2 = strr
-temp = strr[0]
-strr[0] = strr2[len(strr)-1]
-strr2[len(strr)-1] = temp
+if len(strr)>1:
+    strr2 = strr[-1]+strr[1:-1]+strr[0]
+else:
+    strr2 = strr1
 print(f"String after swapping:{strr2}")
