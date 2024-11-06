6c234ca41d5012a678d9a02fd5b19a4cdf5ae093
eswardas <eswardaskn@gmail.com>
2024-11-06 10:46:33 +0000

    Update prog34_dictstudent.py

--- November/nov_06_11_24/prog34_dictstudent.py
+++ November/nov_06_11_24/prog34_dictstudent.py
@@ -5,8 +5,7 @@ grade=''
 for i in range(size):    
     student = {'Name':None,'Roll_number':0,'Register_number':0,'Department':None,'Semester':0,'Math_mark':0,'Lab_mark':0,'DFCA_mark':0,'DS_mark':0,'ASE_mark':0}
     
-    for key in student:
-        
+    for key in student:   
         if key == 'Name' or key == 'Department':
             student[key] = input(f"Enter the {key}: ")
         elif key in ['Roll_number', 'Register_number', 'Semester']:
@@ -21,14 +20,18 @@ for i in range(size):
                     print("Mark should be within 0 and 60..Enter again\n")
                 except ValueError:
                     pass
-            
+
+               
     student['Total_marks'] = 0
     
     for key in ['Math_mark','Lab_mark','DFCA_mark','DS_mark','ASE_mark']:
+        
         student['Total_marks'] += student[key]
+
         
     student['Percentage'] = student['Total_marks']/300*100
     print()
+
     
     if student["Percentage"]>=40:
         
@@ -42,10 +45,11 @@ for i in range(size):
             grade = 'B'
         elif student["Percentage"]>=90:
             grade = 'A'
-        passed_students.update({student['Name']:f'{grade} Grade'})
+        passed_students.update({f"{student['Name']},R.no {student['Roll_number']}":f'{grade} Grade'})
     elif student['Percentage']<40:
         failed_students.append(student['Name'])
     print()
+
 
 print("Passed students:",passed_students)
 print()
