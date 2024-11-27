eb65f754357038a94a93381fe0e59de2963ff5eb
eswardas <eswardaskn@gmail.com>
2024-11-27 06:36:50 +0000

    package added

---
+++ November/nov_27_11_24/package/main.py
@@ -0,0 +1,14 @@
+from  Math_operations.Basic_operations import arithmetic
+from Math_operations.Geometry import area
+
+print("Arithmetic operations:")
+
+arithmetic.add(12,45)
+arithmetic.sub(67,33)
+arithmetic.multiply(23,56)
+arithmetic.divide(100,20)
+
+print("\nGeometric operations:")
+
+area.circle_area(5)
+area.triangle_area(34,56)
