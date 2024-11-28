1d71dc58c9b09f54c6c0ef6d09b6a5b1c468b654
eswardas <eswardaskn@gmail.com>
2024-11-28 20:55:28 +0530

    Update prog21_intcheck.py

--- October/oct18_10_24/prog21_intcheck.py
+++ October/oct18_10_24/prog21_intcheck.py
@@ -1,27 +1,39 @@
-lst1 = []
-len1 = int(input("Enter the size of first list:"))
-print(f"Enter {len1} numbers to the first list:")
-for i in range(len1):
-    temp = int(input())
-    lst1.append(temp)
+while True:
+    print("\nMenu:")
+    print("1. Enter two lists")
+    print("2. Exit")
+    choice = int(input("Enter your choice: "))
 
-lst2 = []
-len2 = int(input("Enter the size of second list:"))
-print(f"Enter {len2} numbers to the second list:")
-for j in range(len2):
-    temp = int(input())
-    lst2.append(temp)
+    if choice == 1:
+        lst1 = []
+        len1 = int(input("Enter the size of first list:"))
+        print(f"Enter {len1} numbers to the first list:")
+        for i in range(len1):
+            temp = int(input())
+            lst1.append(temp)
 
-if len1 == len2:
-    print("Both lists are of same length")
-else:
-    print("Both lists are of different length")
+        lst2 = []
+        len2 = int(input("Enter the size of second list:"))
+        print(f"Enter {len2} numbers to the second list:")
+        for j in range(len2):
+            temp = int(input())
+            lst2.append(temp)
 
-if sum(lst1) == sum(lst2):
-    print("Sum of both lists are same")
-else:
-    print("Sum of both lists are different")
+        if len1 == len2:
+            print("Both lists are of same length")
+        else:
+            print("Both lists are of different length")
 
-for i in lst1:
-    if i in lst2:
-        print(f"'{i}' occurs in both lists")
+        if sum(lst1) == sum(lst2):
+            print("Sum of both lists are same")
+        else:
+            print("Sum of both lists are different")
+
+        for i in lst1:
+            if i in lst2:
+                print(f"'{i}' occurs in both lists")
+
+    elif choice == 2:
+        break
+    else:
+        print("Invalid choice.")
