06f37f302aa7b5b6f1d6b7d67876a7fc3d1e24e5
eswardas <eswardaskn@gmail.com>
2024-12-24 13:52:27 +0000

    Code rebuild

--- November/nov_29_11_24/package/Math_operations/Geometry/area.py
+++
@@ -1,7 +0,0 @@
-from math import pi
-
-def circle_area(radius):
-    print(f"Area of circle(radius:{radius}):{pi * radius * radius}")
-
-def triangle_area(base,height):
-    print(f"Area of triangle(base={base},height={height}):{0.5*base*height}")
--- November/nov_29_11_24/package/main.py
+++ November/nov_29_11_24/package/main.py
@@ -1,14 +1,51 @@
-from  Math_operations.Basic_operations import arithmetic
-from Math_operations.Geometry import area
+from Graphics.circle import *
+from Graphics.rectangle import *
+from Graphics.Graphics_3d import cuboid
+from Graphics.Graphics_3d import sphere
 
-print("Arithmetic operations:")
+while True:
+    print("Select any one of the option(1/2/3/4/5/6/7/8/9)")
+    option = int(input("1.Area of circle\n2.Perimeter of circle\n3.Area of Rectangle\n4.Perimeter of rectangle\n5.Total surface area of cuboid\n6.Volume of cuboid\n7.Total surface area of sphere\n8.Volume of sphere\n9.Exit\nSelect an option: "))
+    if option == 1:
+        radius = float(input("Enter the radius of the circle: "))
+        circle_area(radius)
+        print()
+    elif option == 2:
+        radius = float(input("Enter the radius of the circle: "))
+        circle_perimeter(radius)
+        print()
+    elif option == 3:
+        length = float(input("Enter the length of the rectangle: "))
+        width = float(input("Enter the width of the rectangle: "))
+        rect_area(length, width)
+        print()
+    elif option == 4:
+        length = float(input("Enter the length of the rectangle: "))
+        width = float(input("Enter the width of the rectangle: "))
+        rect_perimeter(length, width)
+        print()
+    elif option == 5:
+        length = float(input("Enter the length of the cuboid: "))
+        breadth = float(input("Enter the width of the cuboid: "))
+        height = float(input("Enter the height of the cuboid: "))
+        cuboid.cuboid_area(length,breadth,height)
+        print()
+    elif option == 6:
+        length = float(input("Enter the length of the cuboid: "))
+        breadth = float(input("Enter the width of the cuboid: "))
+        height = float(input("Enter the height of the cuboid: "))
+        cuboid.cuboid_volume(length,breadth,height)
+        print()
+    elif option == 7:
+        radius = float(input("Enter the radius of the sphere: "))
+        sphere.sphere_area(radius)
+        print()
+    elif option == 8:
+        radius = float(input("Enter the radius of the sphere: "))
+        sphere.sphere_volume(radius)
+        print()
+    elif option == 9:
+        break
 
-arithmetic.add(12,45)
-arithmetic.sub(67,33)
-arithmetic.multiply(23,56)
-arithmetic.divide(100,20)
 
-print("\nGeometric operations:")
 
-area.circle_area(5)
-area.triangle_area(34,56)
